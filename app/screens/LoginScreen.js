95f7b96e924cd64eff353a38c874e52ef87f4226
daniela <delatorre3985@gmail.com>
2022-03-24 14:59:06 -0700

    new_auth

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -7,20 +7,50 @@ export default function LoginScreen( {navigation} ) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    let tokenId = "";
+
+    // const [userTokenId, setUserTokenId] = useState("");
+
+    const request = {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+            "key": "12345678", 
+            "date": "12345678"
+        },
+        // change the body to include the token
+        body: JSON.stringify({ "hello": "hello1"}),
+    };
+    // /auth/<string:database>/<string:collection>/<string:token> 
+    // /auth/appdb/med/token
+
+    const getInfo = async () => {
+        try {
+            await fetch(`http://18.188.94.81/auth/appdb/med/${tokenId}`, request).then((response) => { return response.json(); }).then((myJson) => { console.log(myJson); })
+            
+        } catch (error) {
+            console.error(error);
+        } finally {
+            console.log(tokenId);
+            tokenId = "";
+            // if the user is actually in the database -> navigate to the patient info screen
+            navigation.navigate('VolunteerScreen');
+        }
+    }
+
     const signInAsync = async() => {
         console.log("Login Screen");
 
         const config = {
             issuer: 'https://accounts.google.com',
             clientId: `382563850622-c3q3n26o8mi017qieq7ng9ifdrqivb1o.apps.googleusercontent.com`,
-            redirectUrl: "com.android.ekifubatest.auth://",
+            redirectUrl: "com.android.ekifubatest.auth:/",
             scopes: ["profile", "email"],
           };
 
         try {
             const result = await authorize(config);
-            console.log(result);
-
+            tokenId = result['idToken'];
         } catch (error) {
             console.log("Error with logging in", error);
         }
@@ -57,6 +87,7 @@ export default function LoginScreen( {navigation} ) {
                 <FontAwesome5.Button style={styles.googleButton} backgroundColor="white" name="google" color="green" onPress={signInAsync}>
                 <Text style={styles.googleText}>Log In With Google</Text>
                 </FontAwesome5.Button>
+                <Button color="#b1d8b7" title="After" onPress={getInfo}></Button>
                 <TouchableOpacity style={styles.forgotPassword}>
                     <Text>Forgot Password?</Text>
                 </TouchableOpacity>
