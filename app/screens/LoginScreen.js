9541c4dfa6d631fed3dd1e91842b151cba8170aa
daniela <delatorre3985@gmail.com>
2022-03-31 17:08:38 -0700

    updated working authentication

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,15 +1,95 @@
-import { React, useState } from 'react';
+import { React, useState, useEffect } from 'react';
 import { ImageBackground, View, StyleSheet, Image, Text, Button, TextInput, TouchableOpacity } from 'react-native';
+import {
+    GoogleSignin,
+    GoogleSigninButton,
+    statusCodes,
+} from 'react-native-google-signin';
 
 export default function LoginScreen( {navigation} ) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    loginPress = () => {
+    // adding google authentication here
+    const [loggedIn, setloggedIn] = useState(false);
+    const [userInfo, setuserInfo] = useState([]);
+    const [tokenId, setTokenId] = useState("");
+
+    const signIn = async () => {
+        try {
+            await GoogleSignin.hasPlayServices();
+            const { idToken } = await GoogleSignin.signIn();
+            console.log('Your id token is', idToken);
+            setTokenId(idToken);
+            setloggedIn(true);
+        } catch (error) {
+            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+                // user cancelled the login flow
+                console.log('Cancel');
+            } else if (error.code === statusCodes.IN_PROGRESS) {
+                console.log('Signin in progress');
+                // operation (f.e. sign in) is in progress already
+            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+                console.log('PLAY_SERVICES_NOT_AVAILABLE');
+                // play services not available or outdated
+            } else {
+                // some other error happened
+                console.log(error);
+            }
+        } finally {
+            getInfo();
+        }
+    };
+
+    const request = {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+            "key": "12345678", 
+            "date": "12345678"
+        },
+        // change the body to include the token?
+        body: JSON.stringify({ "hello": "hello1"}),
+    };
+
+    const getInfo = async () => {
+        try {
+            await fetch(`http://18.188.94.81/auth/appdb/med/${tokenId}`, request).then((response) => { return response.json(); }).then((myJson) => { console.log(myJson); })
+        } catch (error) {
+            console.error("The error is", error);
+        } finally {
+            setTokenId("");
+            // if the user is actually in the database -> navigate to the patient info screen
+            navigation.navigate('VolunteerScreen');
+        }
+    }
+
+    const signOut = async () => {
+        try {
+            await GoogleSignin.revokeAccess();
+            await GoogleSignin.signOut();
+            setloggedIn(false);
+            setuserInfo([]);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+      
+    useEffect(() => {
+        GoogleSignin.configure({
+          scopes: ['profile', 'email'], // what API you want to access on behalf of the user, default is email and profile
+          webClientId: '382563850622-mlmd0etlerlhivr31sdcuqq3ccdfg2dk.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+          androidClientId:
+            '382563850622-c3q3n26o8mi017qieq7ng9ifdrqivb1o.apps.googleusercontent.com', 
+        });
+    }, []);
+
+    const loginPress = () => {
         console.log(email);
         console.log(password);
         navigation.navigate('VolunteerScreen');
     } 
+
     return (
         <ImageBackground style={styles.background} source={require("../assets/background.png")}>
             <View style={styles.top}>
@@ -33,6 +113,23 @@ export default function LoginScreen( {navigation} ) {
                     />
                 </View>
                 <Button color="#b1d8b7" title="Login" onPress={loginPress}></Button>
+
+                <View>
+                <GoogleSigninButton
+                    style={styles.googleButton}
+                    size={GoogleSigninButton.Size.Wide}
+                    color={GoogleSigninButton.Color.Light}
+                    onPress={signIn}
+                />
+                </View>
+
+                <View>
+                    <Button
+                    onPress={signOut}
+                    title="LogOut"
+                    color="red"></Button>
+                </View>
+
                 <TouchableOpacity style={styles.forgotPassword}>
                     <Text>Forgot Password?</Text>
                 </TouchableOpacity>
@@ -52,8 +149,10 @@ const styles = StyleSheet.create({
         height: 160,
         bottom: 60,
     },
+    googleButton: {
+    },
     inputsView: {
-        bottom: 20,
+        alignSelf: "center",
     },
     inputs: {
         backgroundColor: "white",
